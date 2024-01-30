7d36fd8a79f3353006186e62c2892d5dedfbc347
NoOnE8151 <152585076+NoOnE8151@users.noreply.github.com>
2024-01-30 17:17:05 +0530

    Update ready.js
    Adds bot status

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,10 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
+	//bot status
+		client.user.setActivity('template by @no_one0521', {
+  type: ActivityType.Playing 
+});
 		console.log(`Logged in as ${client.user.tag} \n template by No OnE#8151.`);
 	},
 };
